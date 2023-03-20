5a581b4b0f417a564286be773edfb0e624e96c26
DianaBulyk <128373584+DianaBulyk@users.noreply.github.com>
2023-03-20 12:48:07 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[]args){
+        System.out.println();
+        int a = 10;
+        int b = 3;
+        int result = a / b;
+        int remainder = a % b;
+
+        System.out.println("Result: " + result);
+        System.out.println("Remainder: " + remainder);
+    }
+}
